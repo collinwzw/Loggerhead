147298545770cd25bdd324f069d553b3075023c6
Ziwen Wang <ziwenwang@Ziwens-MacBook-Pro.local>
2018-05-01 11:51:26 -0400

    modified

--- Loggerhead.py
+++ Loggerhead.py
@@ -1,6 +1,7 @@
 import serial
 import time
 import math
+from math import log10, floor
 
 ser = serial.Serial('/dev/cu.usbmodemFD121',9600)
 count=0
@@ -26,17 +27,26 @@ while True:
         count = count + 1
 
     if count == 5:
-        
-          gyro_x_avg= gyro_x/count
-          gyro_y_avg= gyro_y/count
+          
+          gyro_x_avg = gyro_x/count
+          f"{gyro_x_avg:{3}.{2}}"
+          gyro_y_avg = gyro_y/count
+          format(gyro_y_avg,'.2f')
           gyro_z_avg= gyro_z/count
+          format(gyro_z_avg,'.2f')
           acc_x_avg= acc_x/count
+          format(acc_x_avg,'.2f')
           acc_y_avg= acc_y/count
+          format(acc_y_avg,'.2f')
           acc_z_avg= acc_z/count
+          format(acc_z_avg,'.2f')
+          
           print (gyro_x_avg,gyro_y_avg,gyro_z_avg,acc_x_avg,acc_y_avg,acc_z_avg)
 
           roll = math.atan2(acc_x_avg,acc_z_avg)
+          format(roll,'.2f')
           pitch = math.atan2(acc_y_avg,acc_z_avg)
+          format(pitch,'.2f')
           ser.write(b'm1090100\0')
           gyro_x = 0
           gyro_y = 0
@@ -52,4 +62,3 @@ while True:
 #          speed = 20 #ms delay
 #          ser.write(b'speed')
           count = 0
-
